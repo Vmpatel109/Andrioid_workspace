9d9250201a7eecd55516ecae640970ffc0200c02
Vmpatel109 <136417878+Vmpatel109@users.noreply.github.com>
2023-08-09 11:54:39 +0530

    Add files via upload

---
+++ module2_assignments/assignment2/m2_a2.kt
@@ -0,0 +1,10 @@
+import java.util.*
+
+fun main()
+{
+    val reader = Scanner(System.`in`)
+    print("Enter a number: ")
+    var integer:Int = reader.nextInt()
+    println("You entered: $integer")
+
+}
