713131ea932fde13dd28903b0a6f9021fe8c80a8
Vmpatel109 <136417878+Vmpatel109@users.noreply.github.com>
2023-08-09 11:59:48 +0530

    Add files via upload

---
+++ module2_assignments/assignmet8/m2_a8.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+
+    var n1 = -4.5
+    var n2 = 3.9
+    var n3 = 2.5
+
+    if (n1 >= n2 && n1 >= n3)
+        println("$n1 is the largest number.")
+    else if (n2 >= n1 && n2 >= n3)
+        println("$n2 is the largest number.")
+    else
+        println("$n3 is the largest number.")
+}
