ea191b14b953116a47cb7862e3d3c842dcc4a56d
Vmpatel109 <136417878+Vmpatel109@users.noreply.github.com>
2023-08-09 11:56:32 +0530

    Add files via upload

---
+++ module2_assignments/assignment4/m2_a4.kt
@@ -0,0 +1,12 @@
+fun main()
+{
+    var a = 5.6
+    var b = 6.8
+
+    var multi = a*b
+    println(multi)
+    var c = 'b'
+    var ascii = c.toInt()
+
+    println("The ASCII value of $c is: $ascii")
+}
